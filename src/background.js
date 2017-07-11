1c567ab9b3f65bec90f907b526069a66999141b7
James Forsyth <forsythjames@hotmail.co.uk>
2017-07-11 23:45:35 +0100

    Created repository

---
+++ src/background.js
@@ -0,0 +1,17 @@
+// Only show page_action if the page contains a password field
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			actions: [
+				new chrome.declarativeContent.ShowPageAction()
+			],
+			conditions: [
+				new chrome.declarativeContent.PageStateMatcher({
+					css: [
+						'input[type="password"]'
+					]
+				})
+			]
+		}]);
+	});
+});
